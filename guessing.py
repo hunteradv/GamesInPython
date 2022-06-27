783c9c4c465dfd8fa0aae4bccc2ee26cc7d9e743
HunterAdv <gustavopaulody@gmail.com>
2022-06-27 19:51:58 -0300

    inserindo validação para numero e break caso o número esteja correto

--- guessing.py
+++ guessing.py
@@ -2,13 +2,17 @@ print("*******************************")
 print("Bem-vindo ao jogo de advinhação")
 print("*******************************")
 
-secret_number = 10
+secret_number = 21
 attempts = 3
 
 for actual_round in range(1, attempts + 1):
     print("Tentativa {} de {}".format(actual_round, attempts))
-    guess = int(input("Digite o seu número: "))
+    guess = int(input("Digite um número de 1 a 100: "))
     print("Você digitou ", guess)
+
+    if guess < 1 or guess > 100:
+        print("Você deve digitar um número de 1 a 100!")
+        continue
 
     bigger = guess > secret_number
     smaller = guess < secret_number
@@ -19,5 +23,6 @@ for actual_round in range(1, attempts + 1):
         print("Você errou! Seu chute foi maior que o número secreto")
     else:
         print("Você acertou!")
+        break
 
 print("Fim do jogo")
