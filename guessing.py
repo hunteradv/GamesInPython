4d330934e341a13030032dce33b859cad59dfb41
HunterAdv <gustavopaulody@gmail.com>
2022-06-27 14:58:16 -0300

    Melhorando lógica do jogo

--- guessing.py
+++ guessing.py
@@ -3,14 +3,25 @@ print("Bem-vindo ao jogo de advinhação")
 print("*******************************")
 
 secret_number = 42
+attempts = 3
+actual_round = 1
 
-guess = int(input("Digite o seu número: "))
+while actual_round <= attempts:
+    print("Tentativa ", actual_round, "de ", attempts)
+    guess = int(input("Digite o seu número: "))
+    print("Você digitou ", guess)
 
-print("Você digitou ", guess)
+    bigger = guess > secret_number
+    smaller = guess < secret_number
 
-if guess != secret_number:
-    print("Você errou")
-else:
-    print("Você acertou!")
+    if smaller:
+        print("Você errou! Seu chute foi menor que o número secreto")
+    elif bigger:
+        print("Você errou! Seu chute foi maior que o número secreto")
+    else:
+        print("Você acertou!")
+
+    actual_round = actual_round + 1
+
 
 print("Fim do jogo")
